cb67a7b8ace2c0ca312a4aa2b057faf4224833bd
Eldar <66843118+eldar816@users.noreply.github.com>
2021-06-03 14:35:06 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,2 +1,34 @@
 const cart = [];
 const favorites = [];
+
+function addToCart() {
+
+}
+
+function removeFromCart() {
+
+}
+
+function addToFavorites() {
+
+}
+
+function removeFromFavorites() {
+
+}
+
+function addToWatchlist() {
+
+}
+
+function removeFromWatchlist() {
+
+}
+
+function updateResults() {
+
+}
+
+function searchProducts() {
+
+}
