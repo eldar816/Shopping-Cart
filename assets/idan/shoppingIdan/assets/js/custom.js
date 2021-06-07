66b3969b5b83cf0b24446d07bdf6c7b5cd8d8225
IdanHarari11 <idanharari112@gmail.com>
2021-06-07 23:11:40 +0300

    IdanHarari Changes
    שיניתי כמה דברים בסטייל
    הוספתי את הקטע עם הכוכבים
    סידרתי קצת את הדף של התצוגת מוצר יחיד
    קישרתי את כל הסקריפט שלך לשלי.
    ניסיתי להוסיף לעגלה דרך הכפתור שיש בדף הזה ולא הצלחתי.
    צריך להתעסק שם עם הלוקאל סטוראג
    לא רציתי להתעסק בזה כי כרגע כל המיזוג הזה חוסך לך עבודה אז מקסימום נשלים את השאר יחד.

--- assets/idan/shoppingIdan/assets/js/custom.js
+++ assets/idan/shoppingIdan/assets/js/custom.js
@@ -8,27 +8,29 @@ Author: Eldar
 //     {id : 2, pname : "Product3", price : 150, sizes : "L/XXL", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0}
 // ];
 
-const products = [{id : 0, pname : "Classic Long Sleeve Shirt", price : 50, sizes : "S/M/L/XL/XXL", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 1, pname : "Golf Casual Shirt", price : 100, sizes : "S/M/L/XL/XXL", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 2, pname : "Elegant Daily Dress", price : 150, sizes : "S/M/L/XXL", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 3, pname : "Classic Unique Dress", price : 70, sizes : "S/M/L/XL/XXL", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 4, pname : "Gym One Color Sport Outfit", price : 120, sizes : "S/M/L/XL/XXL", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 5, pname : "Gym Colored DryFit Sport Outfit", price : 80, sizes : "S/M/L/XL/XXL", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 6, pname : "Men's Elegant Blazer", price : 150, sizes : "S/M/L/XL/XXL", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 7, pname : "Men's Casual Daily Blazer", price : 100, sizes : "L/XL/XXL", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 8, pname : "Casual Sunglasses", price : 150, sizes : "S/M/L/XXL", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 9, pname : "Men's Classic Jeans", price : 70, sizes : "S/M/L/XL/XXL", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 10, pname : "Classic Tunique Daily Style", price : 140, sizes : "L/XL/XXL", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 11, pname : "Women's Classic Skinny Jeans", price : 80, sizes : "S/M/L/XL/XXL", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 12, pname : "Unisex Snickers Shoes", price : 150, sizes : "Between 36-43", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 13, pname : "Men's Scribbled Surf Pants Basic ", price : 100, sizes : "L/XL/XXL", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 14, pname : "Elegant Special Events Moccasin", price : 150, sizes : "Between 35-40", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 15, pname : "High Heels Ballet Shoes", price : 80, sizes : "Between 34-38", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 16, pname : "Casual Designed Leather Bag", price : 160, sizes : "Only One Size", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 17, pname : "Women's Elegant Bag For Events", price : 110, sizes : "Only One Size", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0},
-  {id : 18, pname : "Women's Unique Suede Casual Bag", price : 70, sizes : "Only One Size", description : "test1", stars : 4, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 19, pname : "Over-Size Sunglasses Daily Style For Women", price : 120, sizes : "Only One Size", description : "test2", stars : 3, in_cart : false, in_favorites : false, amount : 0}, 
-  {id : 20, pname : "Prestigious Over-Size Sunglasses For Women", price : 80, sizes : "Only One Size", description : "test3", stars : 5, in_cart : false, in_favorites : false, amount : 0}];
+const products = [
+    { id: 0, pname: "Classic Long Sleeve Shirt", price: 50, sizes: "S/M/L/XL/XXL", description: "test1", stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 1, pname: "Golf Casual Shirt", price: 100, sizes: "S/M/L", description: "test2", stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 2, pname: "Elegant Daily Dress", price: 150, sizes: "S/M", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 3, pname: "Classic Unique Dress", price: 70, sizes: "XL/XXL", description: "test1", stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 4, pname: "Gym One Color Sport Outfit", price: 120, sizes: "S/M/XL/XXL", description: "test2", stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 5, pname: "Gym Colored DryFit Sport Outfit", price: 80, sizes: "S/M/L/XL/XXL", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 6, pname: "Men's Elegant Blazer", price: 150, sizes: "S/M/L", description: "test1",type: 'men' ,stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 7, pname: "Men's Casual Daily Blazer", price: 100, sizes: "L/XL/XXL", description: "test2", type: 'men',stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 8, pname: "Casual Sunglasses", price: 150, sizes: "S/M/L/XXL", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 9, pname: "Men's Classic Jeans", price: 70, sizes: "S/M/L/XL/XXL", description: "test1", type: 'men',stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 10, pname: "Classic Tunique Daily Style", price: 140, sizes: "L/XL/XXL", description: "test2", type: 'men',stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 11, pname: "Women's Classic Skinny Jeans", price: 80, sizes: "S/M/L/XL/XXL", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 12, pname: "Unisex Snickers Shoes", price: 150, sizes: "Between 36-43", description: "test1", stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 13, pname: "Men's Scribbled Surf Pants Basic ", price: 100, sizes: "L/XL/XXL", description: "test2", stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 14, pname: "Elegant Special Events Moccasin", price: 150, sizes: "Between 35-40", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 15, pname: "High Heels Ballet Shoes", price: 80, sizes: "Between 34-38", description: "test1", stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 16, pname: "Casual Designed Leather Bag", price: 160, sizes: "Only One Size", description: "test2", stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 17, pname: "Women's Elegant Bag For Events", price: 110, sizes: "Only One Size", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 18, pname: "Women's Unique Suede Casual Bag", price: 70, sizes: "Only One Size", description: "test1", stars: 4, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 19, pname: "Over-Size Sunglasses Daily Style For Women", price: 120, sizes: "Only One Size", description: "test2", stars: 3, in_cart: false, in_favorites: false, amount: 0 },
+    { id: 20, pname: "Prestigious Over-Size Sunglasses For Women", price: 80, sizes: "Only One Size", description: "test3", stars: 5, in_cart: false, in_favorites: false, amount: 0 }
+];
 
 var cart = [];
 var favorites = [];
@@ -39,27 +41,27 @@ let cartCheck = document.getElementById("cartShow");
 let favCheck = document.getElementById("favsShow");
 
 function addToCart(product) {
-        let cart_index;
-        cart = localStorage.getObj('cart');
-        amount = Number(document.getElementById(`_amount${product}`).value);
-        if (cart == null || cart == undefined) {
-            cart = [];
+    let cart_index;
+    cart = localStorage.getObj('cart');
+    amount = Number(document.getElementById(`_amount${product}`).value);
+    if (cart == null || cart == undefined) {
+        cart = [];
+        cart.push(products[product]);
+        cart_index = cart.findIndex(index => index == products[product]);
+        cart[cart_index].amount += amount;
+    } else {
+        cart_index = cart.findIndex(index => index.id == products[product].id);
+        if (cart_index == -1) {
             cart.push(products[product]);
             cart_index = cart.findIndex(index => index == products[product]);
-            cart[cart_index].amount += amount; 
-        } else {
-            cart_index = cart.findIndex(index => index.id == products[product].id);
-            if (cart_index == -1) {
-                cart.push(products[product]);
-                cart_index = cart.findIndex(index => index == products[product]);
-            }
-            cart[cart_index].amount += amount;    
         }
-        localStorage.setObj('cart', cart);
-        cartCount += amount;
-        console.log("ADD TO CART SUCCESS " + amount); 
-        callToast(cart[cart_index].pname + " added to cart " + amount);
-    updatePage(); 
+        cart[cart_index].amount += amount;
+    }
+    localStorage.setObj('cart', cart);
+    cartCount += amount;
+    console.log("ADD TO CART SUCCESS " + amount);
+    callToast(cart[cart_index].pname + " added to cart " + amount);
+    updatePage();
 }
 
 function removeFromCart(product, flag) {
@@ -91,12 +93,12 @@ function removeFromCart(product, flag) {
     } else {
         cart_index = cart.findIndex(index => index.id == products[product].id);
         cart[cart_index].amount -= amount;
-        console.log(cart_index+" cart index");
+        console.log(cart_index + " cart index");
     }
     cartCount -= amount;
     if (cartCount < 0) {
         cartCount = 0;
-    }    
+    }
     console.log("REMOVE FROM CART SUCCESS" + amount);
     callToast("REMOVE FROM CART SUCCESS" + amount);
     localStorage.setObj('cart', cart);
@@ -104,7 +106,7 @@ function removeFromCart(product, flag) {
 }
 
 function removeAllFromCart(product, flag) {
-    removeFromCart(product, flag); 
+    removeFromCart(product, flag);
 }
 
 function addToFavorites(product) {
@@ -114,10 +116,10 @@ function addToFavorites(product) {
         favorites = [];
         favorites.push(products[product]);
         favorites_index = favorites.findIndex(index => index == products[product]);
-        favorites[favorites_index].amount = 1; 
+        favorites[favorites_index].amount = 1;
     } else {
         favorites_index = favorites.findIndex(index => index.id == products[product].id);
-        if (favorites_index == -1) { 
+        if (favorites_index == -1) {
             favorites.push(products[product]);
             favorites_index = favorites.findIndex(index => index == products[product]);
             favorites[favorites_index].amount = 1;
@@ -129,9 +131,9 @@ function addToFavorites(product) {
             return;
         }
 
-    }       
+    }
     localStorage.setObj('favorites', favorites);
-    updatePage(); 
+    updatePage();
 }
 
 function removeFromFavorites(product) {
@@ -150,7 +152,7 @@ function inCart(product) {
         return false;
     } else {
         cart_index = cart.findIndex(index => index == products[product]);
-        if (cart_index !== -1){
+        if (cart_index !== -1) {
             return true;
         }
     }
@@ -163,7 +165,7 @@ function inFavorite(product) {
         return false;
     } else {
         let favorites_index = favorites.findIndex(index => index.id == products[product].id);
-        if (favorites_index !== -1){
+        if (favorites_index !== -1) {
             return true;
         }
     }
@@ -232,8 +234,8 @@ function updatePage() {
         }
         _res.innerHTML += `<div class="col-md-4">
         <div class="card mb-4 product-wap rounded-0">
-        <div class="card rounded-0">
-        <img class="card-img rounded-0 img-fluid" src="assets/img/shop_${products[i].id}.jpg">
+        <div class="card rounded-0" onclick="singlePage${i}">
+        <img class="card-img rounded-0 img-fluid" src="assets/img/shop_${products[i].id}.jpg" >
         <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
         <ul class="list-unstyled">
         ${FavoritesCheck}
@@ -241,7 +243,7 @@ function updatePage() {
         </div>
         </div>
         <div class="card-body">
-        <a href="shop-single.html" class="h3 text-decoration-none">${products[i].pname}</a>
+        <div class="__link"><p class="h3 text-decoration-none" onclick="singlePage(${i})">${products[i].pname}</p></div>
         <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
         <ul class="list-inline pb-3">
         <li class="list-inline-item">Size :
@@ -271,7 +273,7 @@ function updateFavorites() {
     for (let i = 0; i < favorites.length; i++) {
         favCheck.innerHTML += `<div class="col-md-4">
         <div class="card mb-4 product-wap rounded-0">
-            <div class="card rounded-0">
+            <div class="card rounded-0" onclick="singlePage(${i})">
                 <img class="card-img rounded-0 img-fluid" src="assets/img/shop_${favorites[i].id}.jpg">
                 <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                     <ul class="list-unstyled">
@@ -281,8 +283,8 @@ function updateFavorites() {
                     </ul>
                 </div>
             </div>
-            <div class="card-body">
-                <a href="shop-single.html" class="h3 text-decoration-none">${favorites[i].pname}</a>
+            <div class="card-body" onclick="singlePage(${i})">
+                <div class="__link"><p class="h3 text-decoration-none">${products[i].pname}</p></div>
                 <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
                     <li>${favorites[i].sizes}</li>
                 </ul>
@@ -294,14 +296,12 @@ function updateFavorites() {
             </a>
             </div>
         </div>`;
-        }
+    }
 }
 
 function updateCart() {
     cart = localStorage.getObj('cart');
     for (let i = 0; i < cart.length; i++) {
-        console.log(cart);
-        console.log("amount: "+cart[i].amount);
         cartCheck.innerHTML += `<div class="col-md-4">
         <div class="card mb-4 product-wap rounded-0">
             <div class="card rounded-0">
@@ -314,7 +314,7 @@ function updateCart() {
                 </div>
             </div>
             <div class="card-body">
-                <a href="shop-single.html" class="h3 text-decoration-none">${cart[i].pname}</a>
+                <div class="__link"><p class="h3 text-decoration-none" onclick="singlePage(${i})">${products[i].pname}</p></div>
                 <ul class="list-inline pb-3">
                 <li class="list-inline-item">Size :
                 <li>${cart[i].sizes}</li>
@@ -326,7 +326,6 @@ function updateCart() {
                 <p class="text-center mb-0">&dollar;${cart[i].price}</p>
             </div>
         </div>`;
-        console.log(cart[i].amount);
     }
 }
 
@@ -349,10 +348,10 @@ function initPage() {
 }
 
 
-Storage.prototype.setObj = function(key, obj) {
+Storage.prototype.setObj = function (key, obj) {
     return this.setItem(key, JSON.stringify(obj))
 }
-Storage.prototype.getObj = function(key) {
+Storage.prototype.getObj = function (key) {
     return JSON.parse(this.getItem(key))
 }
 
@@ -368,11 +367,11 @@ function updateCounters() {
     for (i in favorites_update) {
         favoritesCount += favorites_update[i].amount;
     }
-    if (favorites_update = null) {   
-        favoritesCount = 0; 
+    if (favorites_update = null) {
+        favoritesCount = 0;
     }
-    if (cart_update = null) {   
-        cartCount = 0; 
+    if (cart_update = null) {
+        cartCount = 0;
     }
 }
 
@@ -400,10 +399,10 @@ function updateCounters() {
 
 function incrementValue(i) {
     document.getElementById(`_amount${i}`).stepUp();
- }
- function decrementValue(i) {
+}
+function decrementValue(i) {
     document.getElementById(`_amount${i}`).stepDown();
- }
+}
 
 function searchProducts() {
 
@@ -415,4 +414,8 @@ function searchResults() {
 
 function getProduct() {
 
+}
+
+function singlePage(i) {
+    location.href = `shop-single.html?i=${i}`;
 }
---
+++ assets/idan/shoppingIdan/assets/js/productsGal.js
@@ -0,0 +1,40 @@
+var categories = [{ id: 1, Categ: "Wallets" }, { id: 2, Categ: "Shirts" }, { id: 3, Categ: "Canvases" }, { id: 4, Categ: "Phone" },
+{ id: 5, Categ: "Accessories" }, { id: 6, Categ: "Other Gifts" },]
+
+var Wallets = [{ name: "Venus", price: "20$", description: "Woman Casual Wallet Artificial Leather Colores Personal dedication", color: "", image: "", count: "" },
+{ name: "Nova", price: "23$", description: "Woman Elegant Wallet Artificial Leather Colores Personal dedication", color: "", image: "", count: "" },
+{ name: "Mark", price: "25$", description: "Man Elegant Wallet Artificial Leather Black/Brown Personal dedication", color: "", image: "", count: "" },
+{ name: "Bolt", price: "20$", description: "Man Sport Wallet Artificial Leather Black/Brown Personal dedication", color: "", image: "", count: "" },
+{ name: "Scopler", price: "30$", description: "Man Elegant Wallet Artificial Leather Black/Brown Personal dedication", color: "", image: "", count: "" },
+{ name: "Diane", price: "35$", description: "Woman Special Events Elegant Wallet Artificial Leather Black/Brown Personal dedication", image: "", count: "" }];
+
+var Shirts = [{ name: "Personal dedication Men's Shirt", price: "12$", description: "Men Style Personal dedication Special Events Black/White T-Shirt", color: "", image: "", count: "" },
+{ name: "Personal dedication Women's Shirt", price: "10$", description: "Women Style Casual Personal dedication Special Events Black/White T-Shirt", color: "", image: "", count: "" },
+{ name: "Personal dedication Men's Tank", price: "10$", description: "Men Style Personal dedication Special Events Black/White Tank", color: "", image: "", count: "" },
+{ name: "Personal dedication Women's Tank", price: "8$", description: "Women Style Personal dedication Special Events Black/White Tank", color: "", image: "", count: "" },
+{ name: "Personal dedication Men's hooded Tank", price: "15$", description: "Men Style Personal dedication Special Events Black/White hooded Tank", color: "", image: "", count: "" },
+{ name: "Personal dedication Women's Tunic", price: "11$", description: "Women Style Personal dedication Special Events Black/White Tunic", color: "", image: "", count: "" }]
+
+var Canvases = [{ name: "20X20 Canvas", price: "19$", description: "Small Canvas Personal dedication 20X20", image: "", count: "" },
+{ name: "20X20X3 On Canvas Image Collage", price: "35$", description: "3 Small Canvases Collage Personal dedication 20X20 Each", image: "", count: "" },
+{ name: "40X40 Canvas", price: "25$", description: "Med-Small Canvas Personal dedication 40X40", image: "", count: "" },
+{ name: "60X80 Canvas", price: "30$", description: "Medium Size Canvas Personal dedication 60X80", image: "", count: "" },
+{ name: "100X70 Canvas", price: "33$", description: "Big Canvas Personal dedication 100X70", image: "", count: "" },
+{ name: "70X100X3 On Canvas Image Collage", price: "50$", description: "3 Big Canvases Collage Personal dedication 70X100 Each", image: "", count: "" }]
+
+var Phone = [{ name: "Iphone Case Personalised Cover", price: "7$", description: "Personalised Cover For Iphone", image: "", count: "" },
+{ name: "Samsung Galaxy Case Personalised Cover", price: "7$", description: "Personalised Cover For Samsung Galaxy", image: "", count: "" },
+{ name: "Xiaomi Case Personalised Cover", price: "7$", description: "Personalised Cover For Xiaomi", image: "", count: "" },
+{ name: "LG Case Personalised Cover", price: "7$", description: "Personalised Cover For LG", image: "", count: "" }]
+
+var Accessories = [{ name: "Bracelet With Name", price: "20$", description: "Personalized Name Bracelet,Silver/Gold Bracelets", color: "", count: "" },
+{ name: "Bracelet Name Engraved", price: "22$", description: "Personalized Engraved Name Bracelet,Silver/Gold Bracelets Leather strip", color: "", count: "" },
+{ name: "Necklace With Name", price: "14$", description: "Personalized Name Necklace,Silver/Gold/Shiny Black", color: "", count: "" },
+{ name: "Bracelet With Name Engraved", price: "18$", description: "Personalized engraved Name Ring,Silver/Gold/Shiny Black", color: "", count: "" }];
+
+var Gifts = [{ name: "Sony PlayStation Remote Personalised Cover", price: "15$", description: "Personalised Cover For PlayStation Remote", image: "", count: "" },
+{ name: "1000 Pieces Puzzle", price: "11$", description: "1000 Pieces Personalised Puzzle With Dedication", image: "", count: "" },
+{ name: "Beer Glass", price: "7$", description: "Personalised Beer Glass", image: "", count: "" },
+{ name: "Coffee Mug", price: "8$", description: "Personalised Coffee Mug", image: "", count: "" }];
+
+var userCart = [];
